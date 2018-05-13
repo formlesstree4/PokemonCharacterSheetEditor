c8545ca8b849e246212025b7fc878163ee5e0227
Adam Michael Sears <asears2@live.com>
2018-05-12 23:50:54 -0500

    Providers WIP.

---
+++ PokemonCharacterSheetEditor.Lib/Providers/TypeProvider.cs
@@ -0,0 +1,28 @@
+﻿using PokemonCharacterSheetEditor.Lib.Database;
+using System.Collections.Generic;
+
+namespace PokemonCharacterSheetEditor.Lib.Providers
+{
+
+    /// <summary>
+    ///     Provides type mappings for Pokemon.
+    /// </summary>
+    public sealed class TypeProvider
+    {
+
+        private readonly IDictionary<int, string> _mappings;
+
+
+        /// <summary>
+        ///     Creates a new instance of the <see cref="TypeProvider"/>
+        /// </summary>
+        /// <param name="query"><see cref="IQueryRunner"/></param>
+        public TypeProvider(IQueryRunner query)
+        {
+            _mappings = new Dictionary<int, string>();
+        }
+
+
+    }
+
+}
