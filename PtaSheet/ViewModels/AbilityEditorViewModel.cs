3973a8e31009bd04c5da5276b654d24d582a0225
Adam Michael Sears <asears2@live.com>
2018-11-24 01:55:53 -0600

    Working on the ability editor

---
+++ PtaSheet/ViewModels/AbilityEditorViewModel.cs
@@ -0,0 +1,129 @@
+﻿using Prism.Events;
+using Prism.Mvvm;
+using PtaSheet.Infrastructure.Events;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace PtaSheet.ViewModels
+{
+    public class AbilityEditorViewModel : BindableBase
+    {
+
+        private ObservableCollection<Model.Ability> _abilities;
+        private Model.Ability _selectedAbility;
+        private Model.PtaConnection _connection;
+        private readonly StatusEvent _statusEvent;
+        private ICommand _addCapabilityCommand;
+        private ICommand _removeCapabilityCommand;
+
+
+        public ObservableCollection<Model.Ability> Abilities
+        {
+            get => _abilities;
+            private set => SetProperty(ref _abilities, value);
+        }
+        public Model.Ability SelectedAbility
+        {
+            get => _selectedAbility;
+            set
+            {
+                if ((!(_selectedAbility is null)) && _connection.ChangeTracker.HasChanges())
+                {
+                    SaveAllChanges();
+                }
+                SetProperty(ref _selectedAbility, value);
+                RaisePropertyChanged(nameof(Activation));
+                RaisePropertyChanged(nameof(Effect));
+                RaisePropertyChanged(nameof(Limit));
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+        public string Activation
+        {
+            get => SelectedAbility.Activation;
+            set
+            {
+                SelectedAbility.Activation = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(SelectedAbility));
+            }
+        }
+        public string Effect
+        {
+            get => SelectedAbility.Effect;
+            set
+            {
+                SelectedAbility.Effect = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(SelectedAbility));
+            }
+        }
+        public string Limit
+        {
+            get => SelectedAbility.Limit;
+            set
+            {
+                SelectedAbility.Limit = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(SelectedAbility));
+            }
+        }
+        public string Name
+        {
+            get => SelectedAbility.Name;
+            set
+            {
+                SelectedAbility.Name = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(SelectedAbility));
+            }
+        }
+        public Model.Keyword Keyword
+        {
+            get => SelectedAbility.Keyword;
+            set
+            {
+                SelectedAbility.Keyword = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(SelectedAbility));
+            }
+        }
+        
+
+
+        public AbilityEditorViewModel()
+        {
+            _abilities = new ObservableCollection<Model.Ability>
+            {
+                new Model.Ability
+                {
+                    Activation = "Instant",
+                    Effect = "None",
+                    Keyword = new Model.Keyword
+                    {
+                        Description = "Haha~",
+                        KeywordId = 0,
+                        Name = "Design Keyword"
+                    },
+                    Limit = "None",
+                    Name = "Designer Ability"
+                }
+            };
+            SelectedAbility = Abilities[0];
+        }
+        public AbilityEditorViewModel(IEventAggregator eventAggregator, Model.PtaConnection connection)
+        {
+            _statusEvent = eventAggregator.GetEvent<StatusEvent>();
+            _connection = connection;
+        }
+
+        private void SaveAllChanges()
+        {
+            _statusEvent.Publish($"Applying changes... please wait...");
+            _connection.SaveChanges();
+            _statusEvent.Publish($"The data source has been updated successfully!");
+        }
+
+    }
+}
---
+++ PtaSheet/Views/AbilityEditor.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PtaSheet.Views
+{
+    /// <summary>
+    /// Interaction logic for AbilityEditor
+    /// </summary>
+    public partial class AbilityEditor : UserControl
+    {
+        public AbilityEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
